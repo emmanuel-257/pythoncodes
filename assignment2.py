7943ac4097b7124e3582e35a4bb3caf946933760
Arap Claqsy <arapclaqsy@Araps-MacBook-Air.local>
2025-03-24 10:46:22 +0300

    initial commit

---
+++ assignment2.py
@@ -0,0 +1,15 @@
+my_list=[10,20,30,40]
+my_list.insert(1, 15)
+print(my_list)
+
+my_list.extend([50,60,70])
+print(my_list)
+
+my_list.pop(7)
+print(my_list)
+
+my_list.sort()
+print(my_list)
+
+x= my_list.index(30)
+print(x)
